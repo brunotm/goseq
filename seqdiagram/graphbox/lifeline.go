de8eafcf006307df83a0c2c2fff12e2b7282b1b4
Bruno Moura <brunotm@gmail.com>
2021-02-12 10:37:33 +0000

    lifeline: use slender contiguous line

--- seqdiagram/graphbox/lifeline.go
+++ seqdiagram/graphbox/lifeline.go
@@ -16,8 +16,8 @@ func (ll *LifeLine) Constraint(r, c int, applier ConstraintApplier) {
 func (ll *LifeLine) Draw(ctx DrawContext, point Point) {
 	s := SvgStyle{}
 	s.Set("stroke", ll.Style.Color)
-	s.Set("stroke-dasharray", "8,8")
-	s.Set("stroke-width", "2px")
+//	s.Set("stroke-dasharray", "8,8")
+	s.Set("stroke-width", "1.5px")
 
 	fx, fy := point.X, point.Y
 	if point, isPoint := ctx.PointAt(ll.TR, ll.TC); isPoint {
